352f29fc69a8a5fa6bd504736cb856650792dab8
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2023-12-21 23:37:08 -0500

    Different query 06

--- src/q06_get_l1_papers_fields.py
+++ src/q06_get_l1_papers_fields.py
@@ -32,33 +32,25 @@ if __name__ == "__main__":
   dr_df = pd.read_csv(join("..", "dr_algorithms_with_ids.csv"))
   dr_corpus_ids = dr_df['ss_corpus_id'].astype(str).tolist()
 
-  # Do a second query which only gets the L1 "citing_corpus_id" column
-  # to pass to the L2 query's .in_() call.
-  l1_corpus_ids = (
-    Citation
-      .select(Citation.citing_corpus_id)
-      .where(Citation.cited_corpus_id.in_(dr_corpus_ids))
-  )
-
   rows = (
-    PaperToField
-      .select(PaperToField.field, PaperToField.source, Paper.citation_count, Paper.year, Paper.title, Paper.venue, Paper.corpus_id, Paper.doi, Citation.cited_corpus_id)
-      .where(Paper.is_preprint == False, Paper.year >= 2013, Paper.corpus_id.in_(l1_corpus_ids))
-      .join(Paper, on=(PaperToField.corpus_id == Paper.corpus_id))
-      .join(Citation, on=(Paper.corpus_id == Citation.citing_corpus_id))
+    Citation
+      .select(Citation.cited_corpus_id, Citation.citing_corpus_id, Paper.citation_count, Paper.year, Paper.title, Paper.venue, Paper.doi, PaperToField.field, PaperToField.source)
+      .where(Citation.cited_corpus_id.in_(dr_corpus_ids), Paper.is_preprint == False, Paper.year >= 2013)
+      .join(Paper, on=(Paper.corpus_id == Citation.citing_corpus_id))
+      .join(PaperToField, on=(Paper.corpus_id == PaperToField.citing_corpus_id))
   )
 
   df = pd.DataFrame(data=[
     {
-      "field": row.field,
-      "source": row.source,
+      "field": row.paper.paper_to_field.field,
+      "source": row.paper.paper_to_field.source,
       "year": row.paper.year,
       "citation_count": row.paper.citation_count,
       "title": row.paper.title,
       "venue": row.paper.venue,
-      "corpus_id": row.paper.corpus_id,
+      "corpus_id": row.citing_corpus_id,
       "doi": row.paper.doi,
-      "cited_corpus_id": get_cited_corpus_id(row)
+      "cited_corpus_id": row.cited_corpus_id
     } for row in rows
   ])
   df.to_csv('q06_get_l1_papers_fields.csv')
